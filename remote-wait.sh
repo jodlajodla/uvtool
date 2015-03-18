82136d9d8e01aa5836135740f4e5b85f131b3c4e
Robie Basak <robie.basak@canonical.com>
2015-03-18 13:48:30 +0000

    Fix wait when guest is systemd

--- remote-wait.sh
+++ remote-wait.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -e
 
-# Wait for runlevel 2
-while [ "$(runlevel|awk '{print $2}')" != 2 ]; do sleep $UVTOOL_WAIT_INTERVAL; done
+# Wait for runlevel 2 (upstart) or 5 (systemd)
+while :; do
+	runlevel=`runlevel|awk '{print $2}'`
+	[ "$runlevel" = 2 -o "$runlevel" = 5 ] && break
+	sleep $UVTOOL_WAIT_INTERVAL
+done
 
 # Wait for cloud-init's signal
 while [ ! -e /var/lib/cloud/instance/boot-finished ]; do sleep $UVTOOL_WAIT_INTERVAL; done
